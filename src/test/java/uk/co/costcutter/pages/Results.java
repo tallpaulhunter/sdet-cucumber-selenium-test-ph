2a209cb7b4be197f8cddedd0c4b6db2df47ee875
Paul Hunter <paul@phunter.co.uk>
2019-06-28 18:17:44 +0100

    refactor(Results.java) - add verification for Location based result count

--- src/test/java/uk/co/costcutter/pages/Results.java
+++ src/test/java/uk/co/costcutter/pages/Results.java
@@ -1,24 +1,29 @@
 package uk.co.costcutter.pages;
 
 import static org.assertj.core.api.Assertions.fail;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 import static uk.co.costcutter.common.CommonVerification.getCommonVerification;
 import static uk.co.costcutter.common.DriverFactory.getChromeDriver;
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.co.costcutter.common.DriverFactory.getWebDriverWait;
 
 import java.util.List;
 import java.util.Map;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import uk.co.costcutter.common.CommonVerification;
 
 public class Results {
 
   protected WebDriver driver = getChromeDriver();
+  protected WebDriverWait wait;
 
   private CommonVerification commonVerification = getCommonVerification();
 
   private By results = By.id("results");
+  private By resultsBanner = By.xpath(("//*[@id=\"totals\"]/div/h3"));
   private By resultsWrapper = By.xpath("//*[@id=\"results\"]/div[1]/div/div");
 
   private Results() {
@@ -34,14 +39,19 @@ public class Results {
     return this;
   }
 
-  public void verifyResultStores(List<Map<String, String>> dataTable) {
+  public Results verifyResultBannerText(int resultCount, String locationText) {
+    commonVerification.verifyItemText(resultsBanner, resultCount + " stores found near");
+    return this;
+  }
+
+  public Results verifyResultStores(List<Map<String, String>> dataTable) {
     int index = 0;
     List<WebElement> resultsList = driver.findElements(resultsWrapper);
     for (Map<String, String> rowData : dataTable) {
       WebElement itemToTest = resultsList.get(index);
       String addressLineAttrib = itemToTest.getAttribute("data-addressline2");
-      String milesAttrib = itemToTest.getAttribute("data-distance"); // '0.54 miles'
-      String expandedCollapsedAttrib = itemToTest.getAttribute("class"); // 'accordion-item open' or 'accordion item'
+      String milesAttrib = itemToTest.getAttribute("data-distance");
+      String expandedCollapsedAttrib = itemToTest.getAttribute("class");
 
       assertThat(addressLineAttrib)
           .as("Address was not as expected ('%s') for row.")
@@ -65,5 +75,15 @@ public class Results {
 
       index++;
     }
+    return this;
+  }
+
+  public Results verifyResultCountNearLocation(int resultCount, String locationText) {
+    wait = getWebDriverWait(5);
+    wait.until(visibilityOfElementLocated(resultsBanner));
+    assertThat(driver.findElement(resultsBanner).getText())
+        .as("The expected number of stores for {} is not as expected", locationText)
+        .isEqualTo(resultCount + " stores found near " + locationText);
+    return this;
   }
 }
