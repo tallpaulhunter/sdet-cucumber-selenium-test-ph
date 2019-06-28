d55cedf4a326d26a42e6cf7fef42ac984bc1b7b1
Paul Hunter <paul@phunter.co.uk>
2019-06-28 22:23:57 +0100

    refactor(TestRunner.java) - Added tag to run all

--- src/test/java/uk/co/costcutter/TestRunner.java
+++ src/test/java/uk/co/costcutter/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = {"src/test/resources/features/storefinder"},
-//    tags = {"@FilteredSearches"},
+    tags = {"@StoreFinder"},
     plugin = {"pretty", "html:target/cucumber"})
 public class TestRunner {
 
